59e20ed0a33ad12f5cbf194310c0d4792d602aac
pamelarussell <pamela.russell@gmail.com>
2016-07-13 11:28:42 -0600

    Add javadoc

--- src/guttmanlab/core/util/SAMFlagDecoder.java
+++ src/guttmanlab/core/util/SAMFlagDecoder.java
@@ -23,6 +23,9 @@ public class SAMFlagDecoder {
 	private int posPcrOrOpticalDuplicate = 10;
 	private int posSupplementaryAlignment = 11;
 	
+	/**
+	 * @param flag The int flag
+	 */
 	public SAMFlagDecoder(int flag) {
 		if(flag < 0) {
 			throw new IllegalArgumentException("Flag must be >= 0");
@@ -31,50 +34,86 @@ public class SAMFlagDecoder {
 		stringLength = binaryString.length();
 	}
 	
+	/**
+	 * @return True if there are multiple segments (not single end)
+	 */
 	public boolean templateHasMultipleSegmentsInSequencing() {
 		return bitIsTrue(posMultipleSegments);
 	}
 	
+	/**
+	 * @return True if each segment is properly aligned
+	 */
 	public boolean eachSegmentProperlyAligned() {
 		return bitIsTrue(posEachSegmentProperlyAligned);
 	}
 	
+	/**
+	 * @return True if this segment is unmapped
+	 */
 	public boolean segmentUnmapped() {
 		return bitIsTrue(posSegmentUnmapped);
 	}
 	
+	/**
+	 * @return True if next segment in template is unmapped
+	 */
 	public boolean nextSegmentInTemplateUnmapped() {
 		return bitIsTrue(posNextSegmentInTemplateUnmapped);
 	}
 	
+	/**
+	 * @return True if this segment is mapped to minus strand
+	 */
 	public boolean seqIsReverseComplemented() {
 		return bitIsTrue(posSeqReverseComplemented);
 	}
 	
+	/**
+	 * @return True if next segment in template is mapped to minus strand
+	 */
 	public boolean seqOfNextSegmentInTemplateIsReverseComplemented() {
 		return bitIsTrue(posSeqOfNextSegmentInTemplateReversed);
 	}
 	
+	/**
+	 * @return True if this is the first segment in template
+	 */
 	public boolean firstSegmentInTemplate() {
 		return bitIsTrue(posFirstSegmentInTemplate);
 	}
 	
+	/**
+	 * @return True if this is the last segment in template
+	 */
 	public boolean lastSegmentInTemplate() {
 		return bitIsTrue(posLastSegmentInTemplate);
 	}
 	
+	/**
+	 * @return True if this is not the primary alignment
+	 */
 	public boolean secondaryAlignment() {
 		return bitIsTrue(posSecondaryAlignment);
 	}
 	
+	/**
+	 * @return True if this mapping is marked as a PCR or optical duplicate
+	 */
 	public boolean pcrOrOpticalDuplicate() {
 		return bitIsTrue(posPcrOrOpticalDuplicate);
 	}
 	
+	/**
+	 * @return True if this mapping does not pass quality controls
+	 */
 	public boolean notPassingQualityControls() {
 		return bitIsTrue(posNotPassingQualityControls);
 	}
 	
+	/**
+	 * @return True if this is a supplementary alignment
+	 */
 	public boolean supplementaryAlignment() {
 		return bitIsTrue(posSupplementaryAlignment);
 	}
