454e5ed0342e53d5069bbdb032bc552d4377dc52
pamelarussell <pamela.russell@gmail.com>
2016-07-13 11:26:28 -0600

    Add the ability to filter records before serializing. Also fixed a bug involving incorrect repeated use of some record attributes.

--- src/guttmanlab/core/serialize/sam/SerializeBam.java
+++ src/guttmanlab/core/serialize/sam/SerializeBam.java
@@ -1,10 +1,10 @@
 package guttmanlab.core.serialize.sam;
 
-import guttmanlab.core.util.CommandLineParser;
-
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.apache.avro.AvroRuntimeException;
 import org.apache.avro.Schema;
@@ -31,21 +31,33 @@ public class SerializeBam {
 	private static Logger logger = Logger.getLogger(SerializeBam.class.getName());
 	
 	/**
+	 * Get whether a record passes all of a collection of filters
+	 * @param record The record
+	 * @param filters The filters
+	 * @return True if the record passes all filters or the collection of filters is empty
+	 */
+	private static <T extends Object> boolean passesAll(T record, Collection<Predicate<T>> filters) {
+		return filters.stream().allMatch(predicate -> predicate.test(record));
+	}
+	
+	
+	
+	/**
 	 * Write the avro file
 	 * @param schemaFile Avro schema file with .avsc extension
 	 * @param inputBam Bam file to serialize
 	 * @param outputAvro Avro file to write
 	 * @throws IOException
 	 */
-	public static void serialize(String schemaFile, String inputBam, String outputAvro) throws IOException {
+	public static void serialize(String schemaFile, String inputBam, String outputAvro, Collection<Predicate<SAMRecord>> filters) throws IOException {
+		
+		logger.info("Serializing " + inputBam + "...");
 		
 		// Reader for bam file
 		SAMFileReader samReader = new SAMFileReader(new File(inputBam));
 		SAMRecordIterator samIter = samReader.iterator();
 		// Create the schema
 		Schema schema = new Schema.Parser().parse(new File(schemaFile));
-		// Create a record to hold sam record
-		GenericRecord avroRec = new GenericData.Record(schema);
 		// This file will have Avro output data
 		File AvroFile = new File(outputAvro);
 		// Create a writer to serialize the record
@@ -54,9 +66,15 @@ public class SerializeBam {
 		dataFileWriter.setCodec(CodecFactory.snappyCodec());
 		dataFileWriter.create(schema, AvroFile);
 		
+		int numDone = 0;		
 		// Iterate over bam file and write to Avro output file
 		while(samIter.hasNext()) {
+			numDone++;
+			if(numDone % 100000 == 0) logger.info("Finished " + numDone + " records");
 			SAMRecord samRecord = samIter.next();
+			if(!passesAll(samRecord, filters)) continue;
+			// Create a record to hold sam record
+			GenericRecord avroRec = new GenericData.Record(schema);
 			avroRec.put("qname", samRecord.getReadName());
 			avroRec.put("flag", samRecord.getFlags());
 			avroRec.put("rname", samRecord.getReferenceName());
@@ -74,8 +92,10 @@ public class SerializeBam {
 				try {
 					avroRec.put(name, tag.value);
 				} catch(AvroRuntimeException e) {
-					// TODO keep track of these and report
-					// This exception is thrown if the schema does not contain the tag name
+					samReader.close();
+					dataFileWriter.close();
+					e.printStackTrace();
+					throw new IllegalStateException("Error probably caused by the fact that schema does not contain tag " + tag.tag);
 				}
 			}
 			dataFileWriter.append(avroRec);
@@ -86,19 +106,5 @@ public class SerializeBam {
 		
 	}
 	
-	public static void main(String[] args) throws IOException {
-		long start = System.nanoTime();
-		CommandLineParser p = new CommandLineParser();
-		p.addStringArg("-s", "Avro schema file with .avsc extension", true);
-		p.addStringArg("-b", "Input bam file", true);
-		p.addStringArg("-a", "Output Avro file with .avro extension", true);
-		p.parse(args);
-		String schema = p.getStringArg("-s");
-		String bam = p.getStringArg("-b");
-		String avro = p.getStringArg("-a");
-		serialize(schema, bam, avro);
-		long time = (System.nanoTime() - start) / 1000000000;
-		logger.info("Total time: " + time + " seconds");
-	}
 	
 }
